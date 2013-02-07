3d0e2adcae5d87979047862c1402b30e5caaf543
Philippe Jayet <philippe@easybox.ch>
2013-02-07 19:13:11 +0100

    upgraded redis-client to version 2.10

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "net.debasishg" %% "redisclient" % "2.8"
+      "net.debasishg" %% "redisclient" % "2.10"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
